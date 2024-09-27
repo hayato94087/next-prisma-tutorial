4e61374220d9204bb9ad49ef77e7b0fbb25cc803
hayato94087 <hayato94087@gmail.com>
2024-09-27 20:25:08 +0900

    Prisma Client を利用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import { type FC } from "react";
+import { db } from "@/server/db";
 
-const Home: FC = () => {
+const Home: FC = async () => {
+  const users = await db.user.findMany();
+
   return (
     <main>
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello World</h1>
+          <br />
+          {users.map((user) => (
+            <div key={user.id} className="text-gray-500 text-sm">
+              {user.name},{user.email}
+            </div>
+          ))}
         </div>
       </div>
     </main>
